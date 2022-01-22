98ed8b9918abd95133c709d985f9850df6b831ce
Michal <18263943+wbsth@users.noreply.github.com>
2022-01-22 20:54:48 +0100

    Add project files.

---
+++ rbr-rt/ViewModels/SetupExtractorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using rbr_rt.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rbr_rt.ViewModels
+{
+    public class SetupExtractorViewModel:ObservableObject
+    {
+        private string _replayPath;
+        public string ReplayPath 
+        {
+            get => _replayPath;
+            set
+            {
+                if (_replayPath != value)
+                {
+                    _replayPath = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
